dd27085c2c515fd6cc8848246fa12102f2ea0f75
kavyaMVG <kavya.mvg@gmail.com>
2023-11-09 19:52:12 +0530

    Styled header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,39 @@
+import { AppBar, Toolbar, Typography, Box, InputBase} from '@material-ui/core'
 import React from 'react'
+import useStyles from './styles'
+// import { Autocomplete } from '@material-ui/lab';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+
 
 const Header = () => {
+    const classes = useStyles();
   return (
-    <div>
-       <h1>Header</h1> 
-    </div>
+    <AppBar position='static'>
+        <Toolbar className={classes.toolbar}>
+            <Typography variant='h6' className={classes.title}>
+                Travel Advisor
+            </Typography>
+            <Box  style={{display:'flex', alignItems:'center'}}>
+                <Typography variant='h6' className={classes.title}>
+                    Explore New Places
+                </Typography>
+                {/* <Autocomplete> */}
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase placeholder='Search....' classes={{root:classes.inputInput}}/>
+
+                    </div>
+                {/* </Autocomplete> */}
+
+            </Box>
+
+        </Toolbar>
+
+    </AppBar>
   )
 }
 
