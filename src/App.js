f95da727b69837fb1d42b786f3d568385dd12090
kavyaMVG <kavya.mvg@gmail.com>
2023-11-15 18:03:22 +0530

    Added map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Map from "./components/Map/Map";
 import List from "./components/List/List";
 // import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
@@ -6,6 +6,22 @@ import Header from "./components/Header/Header";
 import { CssBaseline, Grid } from "@material-ui/core";
 
 const App = () => {
+  const [coords, setCoords] = useState({});
+  //   const [type, setType] = useState("restaurants");
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const [bounds, setBounds] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    console.log(bounds, setWeatherData);
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoords({ lat: latitude, lng: longitude });
+      }
+    );
+  }, [bounds]);
+
   return (
     <>
       <CssBaseline />
@@ -15,7 +31,12 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setBounds={setBounds}
+            setCoords={setCoords}
+            coords={coords}
+            weatherData={weatherData}
+          />
         </Grid>
       </Grid>
     </>
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,30 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import useStyles from "./styles.js";
 
+// const containerStyle =
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+const coords = { lat: 0, lng: 0 };
 const Map = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        center={center}
+        coords={coords}
+        bootstrapURLKeys={process.env.MAP_API_KEY}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+        }}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
     </div>
   );
 };
